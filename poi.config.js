159b482daf0fe2e25dbc8966ae141c78c87cf873
edgji <j.edgji@gmail.com>
2017-10-17 01:31:29 -0700

    add poi config with package alias

---
+++ poi.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports = {
+  extendWebpack(config) {
+    config.resolve
+      .alias
+        .set('package', path.resolve(__dirname, '../package.json'))
+  }
+}
