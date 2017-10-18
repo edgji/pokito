5d44a4af713cdeb2e6da82af85c922c6312d5b79
edgji <j.edgji@gmail.com>
2017-10-18 00:41:45 -0700

    add firebase config and vuexfire mutations

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 // import * as getters from './getters'
 // import createLogger from 'vuex/dist/logger'
-// import firebaseMutations from 'vuexfire'
+import { firebaseMutations } from 'vuexfire'
 
 Vue.use(Vuex)
 
@@ -10,8 +10,8 @@ export default new Vuex.Store({
   state: {
   },
   // getters,
-  //   mutations: {
-  //   ...firebaseMutations
-  // },
+  mutations: {
+    ...firebaseMutations
+  },
   // plugins: [createLogger()]
 })
