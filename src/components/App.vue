d990f0ce5caca56df7be2aba786a5382be28a76f
edgji <j.edgji@gmail.com>
2017-10-17 20:54:58 -0700

    add bulma nav and logo

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <h1>Hello {{ name.replace('-', ' ') }}</h1>
+  <nav class="navbar is-white" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item brand-text logo">{{ name }}</a>
+      </div>
+    </div>
+  </nav>
 </template>
 <script>
   export default {
-    data() {
-      return this.$store.state.pkg
+    computed: {
+      name() {
+        return this.$store.state.pkg.name.replace('-', ' ')
+      }
     }
   }
 </script>
+
 <style lang="scss">
   @import '~bulma';
   @import url('https://fonts.googleapis.com/css?family=Rubik');
+
+  .logo {
+    font-family: 'Rubik', sans-serif;
+    text-transform: uppercase;
+  }
 </style>
