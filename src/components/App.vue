0cb94f82bd5ca5638739366c8f14e2f00e9196f3
edgji <j.edgji@gmail.com>
2017-10-18 00:48:40 -0700

    add svg logo

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,7 +2,9 @@
   <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
-        <a class="navbar-item brand-text logo">{{ name }}</a>
+        <a class="navbar-item brand-text">
+          <img src="static/logo.svg" alt="Pokito Poker">
+        </a>
       </div>
     </div>
   </nav>
@@ -10,19 +12,10 @@
 <script>
   export default {
     computed: {
-      name() {
-        return this.$store.state.pkg.name.replace('-', ' ')
-      }
     }
   }
 </script>
 
 <style lang="scss">
   @import '~bulma';
-  @import url('https://fonts.googleapis.com/css?family=Rubik');
-
-  .logo {
-    font-family: 'Rubik', sans-serif;
-    text-transform: uppercase;
-  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import pkg from 'package'
 // import * as getters from './getters'
 // import createLogger from 'vuex/dist/logger'
 // import firebaseMutations from 'vuexfire'
@@ -9,7 +8,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    pkg
   },
   // getters,
   //   mutations: {
