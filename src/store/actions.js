14041c38e9ba3e16708d229ca1ef4f3371ec8c18
edgji <j.edgji@gmail.com>
2017-10-20 00:01:01 -0700

    game and story action updates - update addGame action - add addStory action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,30 @@
 import { firebaseAction } from 'vuexfire'
+import { database } from 'firebase'
 import app from '@/firebase'
 
 export const setGamesRef = firebaseAction(({ bindFirebaseRef }, ref) => bindFirebaseRef('games', ref))
+
 export const initGamesRef = ({ dispatch }) => {
   const ref = app.database().ref('games')
   dispatch('setGamesRef', ref)
 }
 
-export const addGame = () => {
+export const addGame = ({ name }) => {
   app.database()
     .ref('games')
     .push({
-      title: new Date().toDateString()
+      name: name || '',
+      users: [],
+      stories: [],
+      created: database.ServerValue.TIMESTAMP
+    })
+}
+
+export const addStory = ({ gameId, name }) => {
+  app.database()
+    .ref(`games/${gameId}/stories`)
+    .push({
+      name: name || '',
+      points: ''
     })
 }
