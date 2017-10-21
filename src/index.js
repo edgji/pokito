9102b81c5a49452e62f0f26f56bf2fda47d6e907
edgji <j.edgji@gmail.com>
2017-10-20 22:19:26 -0700

    add vue-router-sync

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './components/App'
 import store from './store'
 import router from './router'
+import { sync } from 'vuex-router-sync'
+
+sync(store, router)
 
 new Vue({
   el: '#app',
